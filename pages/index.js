f9b353fb2a5e0f5d3d9c7bc7b2ed9827b33c9ae7
Matias Herranz <matias@ipsy.com>
2020-06-13 17:41:55 -0300

    Updated index file.

--- pages/index.js
+++ pages/index.js
@@ -45,8 +45,8 @@ export default function Home({ allPostsData }) {
           Here's a <a href="https://www.linkedin.com/in/matiasherranz" target="_blank">link</a> to my profile.
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website - you can building one a site like this with the{' '}
+          <a href="https://nextjs.org/learn">Next.js tutorial!</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
